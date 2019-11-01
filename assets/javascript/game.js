f3b4a1356f556d72ee7bf79bc092f1925347a10c
Taylor Zettel <tzettel@gmail.com>
2019-11-01 15:41:11 -0400

    populated gemArr array with 4 random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
+  
   var randNum = 0;
   var score = 0;
   var gemArr = [];
   
   function randNumPop() {
     randNum = randGen(19, 120);
-    console.log(randNum);
+    console.log("Random Number - " + randNum);
     $("#rand-num-dis").html(randNum);
     return randNum;
+  }
+
+  function gemPop() {
+    for (var i = 0; i < 4; i++) {
+      var gemNum = randGen(1, 12);
+      gemArr.push(gemNum);
+      console.log("Gem " + (i+1) + " - " + gemNum);
+    }
   }
 
   function reset() {
     var score = 0;
     var gemArr = [];
     randNumPop();
+    gemPop();
   }
 
   function randGen(min, max) {
